2b392ab08d5d6f1548825e66111ce5bac89f868a
pbrookeschambers <pbrookeschambers@gmail.com>
2023-07-04 10:41:07 +0100

    Shuffled imports

--- src/qoplots/__main__.py
+++ src/qoplots/__main__.py
@@ -1,34 +1,34 @@
 
 
 if __name__ == "__main__":
-    from .qoplots import init, show_scheme
+    from .qoplots import show_scheme, set_scheme
     import sys
     args = sys.argv[1:]
     if len(args) == 0:
         print("Usage: qoplots [scheme] [variant]")
         quit()
     if len(args) == 1:
-        init(args[0])
+        set_scheme(args[0])
         show_scheme(name = f"{args[0]} (light)")
-        init(args[0], "dark")
+        set_scheme(args[0], "dark")
         show_scheme(name = f"{args[0]} (dark)")
     if len(args) == 2:
         if args[1].lower() == "both":
-            init(args[0])
+            set_scheme(args[0])
             show_scheme(name = f"{args[0]} (light)")
-            init(args[0], "dark")
+            set_scheme(args[0], "dark")
             show_scheme(name = f"{args[0]} (dark)")
         elif args[1].lower() == "save":
-            init(args[0])
+            set_scheme(args[0])
             show_scheme(name = f"{args[0]} (light)", save = True)
-            init(args[0], "dark")
+            set_scheme(args[0], "dark")
             show_scheme(name = f"{args[0]} (dark)", save = True)
         else:
-            init(args[0], args[1])
+            set_scheme(args[0], args[1])
             show_scheme(name = f"{args[0]} ({args[1]})")
     if len(args) == 3:
         if args[2].lower() == "save":
-            init(args[0], args[1])
+            set_scheme(args[0], args[1])
             show_scheme(name = f"{args[0]} ({args[1]})", save = True)
         else:
             print("Usage: qoplots [scheme] [variant] [save]")
