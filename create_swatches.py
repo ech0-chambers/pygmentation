4075f8d4dace9e7ee7624e402ce14dcd709ad991
Peter Brookes Chambers <pbrookeschambers@gmail.com>
2023-11-06 11:19:10 +0000

    fixed README swatches -- again

--- create_swatches.py
+++ create_swatches.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 pre_file = Path("pre_README.md")
 post_file = Path("README.md")
+swatches_dir = Path("./.swatches")
 
 if not pre_file.exists():
     print("No pre file found.")
@@ -10,6 +11,9 @@ if not pre_file.exists():
 
 if post_file.exists():
     post_file.unlink()
+
+if not swatches_dir.exists():
+    swatches_dir.mkdir()
 
 
 # Read in the pre file
@@ -24,9 +28,12 @@ swatch_re = re.compile(r"swatch\((?P<background>#[0-9a-fA-F]{6})(?:,\s*?(?P<text
 svg_template = """<svg xmlns="http://www.w3.org/2000/svg" width="80" height="20" viewBox="0 0 80 20"><rect width="80" height="20" rx = "5" ry = "5" fill="{background}" /><text x="50%" y="50%" dy=".3em" fill="{text}" text-anchor="middle" font-family="monospace" font-size="11">{background}</text></svg>"""
 
 with open(post_file, "w") as f:
-    for match in swatch_re.finditer(contents):
-        background = match.group("background")
-        text = match.group("text") or "#000000"
+    for i, m in enumerate(swatch_re.finditer(contents)):
+        background = m.group("background")
+        text = m.group("text") or "#000000"
         svg = svg_template.format(background=background, text=text)
-        contents = contents.replace(match.group(0), svg)
+        svg_file = swatches_dir / f"{i}.svg"
+        with open(svg_file, "w") as f2:
+            f2.write(svg)
+        contents = contents.replace(m.group(0), f"![{background}](./.swatches/{i}.svg)")
     f.write(contents)
